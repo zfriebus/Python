ab99e960b5a8b45ed715803a36bb26d66b266a66
zfriebus <zjfriebus@gmail.com>
2024-08-26 23:45:03 -0500

    Add files via upload

---
+++ interger.py
@@ -0,0 +1,23 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 13
+print("Literal:", x)
+
+# Integer
+y = -100
+print("Integer:", y)
+
+# Float
+z = 19
+print("Float:", z)
+
+# String
+name = "Zach"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
+ 
