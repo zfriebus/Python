e34d29fdc05ed7626096dde51401371813ba2262
zfriebus <zjfriebus@gmail.com>
2024-09-02 20:58:51 -0500

    Add files via upload

---
+++ python comments.py
@@ -0,0 +1,11 @@
+
+#Prints the "miles per gallon" then prints the number for miles per gallon
+
+# The size of the tank will be 15
+tank_size = 15
+# the distance will be 600 
+distance = 600
+# The miles per gallon= the distance/ tank size
+mpg = distance / tank_size
+#Prints the "miles per gallon" then prints the number for miles per gallon
+print("Miles per gallon:", mpg)
