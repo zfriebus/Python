142b3ad97578669a685841933bc38f3b4dbcf9d4
zfriebus <zjfriebus@gmail.com>
2024-09-02 20:52:36 -0500

    Add files via upload

---
+++ PythonVariablesandFormattingNumbers.py
@@ -0,0 +1,13 @@
+
+
+kilograms_1 = 5
+
+# Formula, Pounds (lb) = Kilograms (kg) * 2.20462
+
+conversion_factor = 2.20462
+
+
+pounds_1 = kilograms_1 * 2.20462
+
+
+print(f"{kilograms_1} pounds is equal to {pounds_1:.4f} pounds.")
