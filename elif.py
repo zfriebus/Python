09323df390dfebed583e18b60aa8ae7bc596ed9c
zfriebus <zjfriebus@gmail.com>
2024-09-09 22:02:21 -0500

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,11 @@
+grade = int(input("What is your grade?  "))
+if grade >= 90 and grade <= 100:
+    print("Your grades a A.")
+if grade >= 80 and grade <= 89:
+    print("Your grades a B.")
+if grade >= 70 and grade <= 79:
+    print("Your grades a C.")
+if grade >= 60 and grade <= 69:
+    print("Your grades a D.")
+if grade <= 59:
+    print("Your grades a F.")
