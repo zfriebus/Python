0fe93ad25c7da616cb99cc6bd986c46d4a68e075
zfriebus <zjfriebus@gmail.com>
2024-09-09 21:46:54 -0500

    Add files via upload

---
+++ MakingDecisionsInPython.py
@@ -0,0 +1,17 @@
+age = float(input("Whats your age? "))
+if age >= 17:
+    print("You can drive.")
+else:
+    print("you cant drive.")
+if age >= 18:
+    print("You can vote.")
+else:
+    print("you cant vote.")
+if age >= 21:
+    print("You can buy alcohol.")
+else:
+    print("You cant buy alcohol.")
+if age >= 65:
+    print("you can get a senior discount.")
+else:
+    print("You cant get the senior discount.")
