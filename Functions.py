673c11b9a11618f12cb01686d20fac768a54bf49
zfriebus <zjfriebus@gmail.com>
2024-09-23 22:36:37 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -4,4 +4,7 @@ def Square(side):
     print(f"The area of the square is ",Area," square units")
 
 Square(4)
- #def Circle(radius)
+def Circle(radius):
+    CircleArea =  3.14 * radius * radius
+    print(f"The area of the circle is ",CircleArea," square units")
+Circle(5)
