4bb4abafc88ea7d9bc1489609a379b476cdc8811
zfriebus <zjfriebus@gmail.com>
2024-10-01 00:18:13 -0500

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,6 @@
+def main():
+   programming_classes = ("Intro to Python", "Advanced Python", "Database Essentials", "Web Development Basics", "Data Structures in Python","Web Design Fundamentals") 
+   for item in programming_classes: 
+       print(item) 
+   print("There are", len(programming_classes), "classes.") 
+main()
