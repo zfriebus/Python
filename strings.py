c50c3b595c34cf952bc54815d8527a5ff47c477e
zfriebus <zjfriebus@gmail.com>
2024-10-21 19:24:43 -0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,17 @@
+def main():
+# Example string
+ example_string = "Hello,Pythonprogrammers"
+ print(example_string)
+ ascii_value = (example_string)
+ string = "Hello,Python programmers"
+ list_of_chars = list(string)
+ print(list_of_chars)
+ min_char = min(example_string)
+ max_char = max(example_string)
+ print(min_char)
+ print(max_char)
+ print(example_string.index("o"))
+ print(example_string.count("o"))
+# TODO: Find and print the index of the first occurrence of 'o' in the string
+# print("\nIndex of 'o':")
+main()
